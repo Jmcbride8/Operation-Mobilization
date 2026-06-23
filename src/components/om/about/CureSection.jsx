af70b0e8ec02b14635759694bbebab59bebdaabb
Jason McBride <jmcbride.isye@gmail.com>
2026-06-23 05:17:17 +0000

    File changes

--- src/components/om/about/CureSection.jsx
+++ src/components/om/about/CureSection.jsx
@@ -59,7 +59,7 @@ export default function CureSection() {
               initial={{ opacity: 0, y: 30 }}
               animate={isInView ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 0.5, delay: i * 0.15 }}
-              className="relative p-8 md:p-12 space-y-6 group overflow-hidden"
+              className="relative p-8 md:p-12 space-y-6 group overflow-hidden min-h-[600px]"
             >
               {/* Background image */}
               <div className="absolute inset-0">
